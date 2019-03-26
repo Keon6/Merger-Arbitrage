6c5fad0524f66e04a6fec23b28b3eaf3b103c420
kevin <kevinkim1207@gmail.com>
2019-03-26 12:54:30 -0500

    3rd commit - scrubbing data more and column renaming, 1-hot encoding of categorical vars

--- Exploratory Analysis.py
+++ Exploratory Analysis.py
@@ -45,26 +45,33 @@ intl_data_years = []
 # INTL_MERGER_DATA.columns = [name.replace("\n", " ") for name in INTL_MERGER_DATA.columns]
 
 
-# II: Remove "unnecessary" columns
+# II: Data Scrubbing
+# -> Remove "unnecessary" columns
 US_MERGER_DATA = US_MERGER_DATA.drop(
     ["  Date Withdrawn", "  Date Effective", "Date Effective/ Unconditional", "Target Company  Date   of  Fin.",
      "History File Event", "Target Name", "Acquiror Name"], axis=1
 )
 
-# print("---- Shape ----")
-# print(US_MERGER_DATA.shape)
-# # print("---- Col Names ----")
-# # print(US_MERGER_DATA.columns)
-# print("---- Null Count ----")
-# print(US_MERGER_DATA.isnull().sum())
+# -> Drop points with missing labels
+US_MERGER_DATA = US_MERGER_DATA.drop(US_MERGER_DATA[US_MERGER_DATA['Status'].isnull()].index.tolist())
 
-# Convert data types for each column as appropriate
-colnames = US_MERGER_DATA.columns
+# -> 1-hot encoding for categorical variables
+categorical_cols = ["Target Industry Sector", "Acquiror Industry Sector", "Target Nation", "Acquiror  Nation"]
+new = pd.get_dummies(US_MERGER_DATA[categorical_cols], drop_first=False)
+for new_col in new.columns:
+    US_MERGER_DATA[new_col] = new[new_col]
+US_MERGER_DATA = US_MERGER_DATA.drop(columns=categorical_cols)
+del new
+
+# status = set()
+# for i in US_MERGER_DATA.index:
+#     status.add(US_MERGER_DATA.at[i, "Status"])
+# print(status)
+
+# -> Convert data types for each column as appropriate
 US_MERGER_DATA[" Rank Date"] = pd.to_datetime(US_MERGER_DATA[" Rank Date"])
 US_MERGER_DATA["  Date Announced"] = pd.to_datetime(US_MERGER_DATA["  Date Announced"])
 colnames_dtype_map = {
-    "Target Industry Sector": "category",
-    "Acquiror Industry Sector": "category",
     "Status": "category",
     "Value of Transaction ($mil)": "float64",
     "Ranking Value inc. Net Debt of Target ($Mil)": "float64",
@@ -82,12 +89,9 @@ colnames_dtype_map = {
     'Target Common Equity ($mil)': "float64",
     'Ratio of Offer Price to EPS': "float64"
 }
-
 for col, dtype in colnames_dtype_map.items():
     # replace all commas in numbers and 'np' and 'nm' values to null
     if dtype == "float64":
-        # US_MERGER_DATA.loc[US_MERGER_DATA[col] == "np"] = np.nan
-        # US_MERGER_DATA.loc[US_MERGER_DATA[col] == "nm"] = np.nan
         for i in US_MERGER_DATA.index:
             if type(US_MERGER_DATA.at[i, col]) is float:
                 pass
@@ -98,11 +102,44 @@ for col, dtype in colnames_dtype_map.items():
                     US_MERGER_DATA.at[i, col] = US_MERGER_DATA.at[i, col].replace(",", "")
     US_MERGER_DATA[col] = US_MERGER_DATA[col].astype(dtype)
 
+# -> Rename columns to make them more legible
+US_MERGER_DATA.rename(columns={
+    ' Rank Date': 'Rank Date',
+    '  Date Announced': 'Announced Date',
+    ' % of Shares Acq.': '% of Shares Acquired',
+    '  % Owned After Trans- action': '% Owned After Transaction',
+    '  % sought': '% Sought',
+    'Value of Transaction ($mil)': 'Transaction Value',
+    'Ranking Value inc. Net Debt of Target ($Mil)': 'Target Net Debt',
+    'Enterprise   Value   ($mil)': 'Target Enterprise Value',
+    '  Equity Value      ($mil)': 'Target Equity Value',
+    'Target  Net Sales  LTM ($mil)': 'Target  Net Sales (YTD)',
+    'EBIT Last Twelve Months ($ Mil)': 'Target EBIT (YTD)',
+    'Pre-tax Income Last Twelve Months ($ Mil)': 'Target Pre-Tax Income (YTD)',
+    'Net Income Last Twelve Months ($ Mil)': 'Target Net Income (YTD)',
+    'Target  Net Assets ($mil)': 'Target Net Asset',
+    'Target Total Assets ($mil)': 'Target Total Asset',
+    'Target  EBITDA  LTM ($mil)': 'Target EBITDA (YTD)',
+    'Target Book Value Per Share LTM (US$)': 'Target Book Value/Share (YTD)',
+    'Target Common Equity ($mil)': 'Target Common Equity',
+    'Target Earnings Per Share LTM (US$)': 'Target EPS (YTD)',
+    'Ratio of Offer Price to EPS': 'Offer Price / EPS',
+    'Target Total  Fees ($mil)': 'Target Total Adviser Fee',
+    'Acquiror  Total   Fees  ($mil)': 'Acquiror Total Advisor Fee',
+    'Target Share Price 1 Day Prior to Announcement ($)': 'Target Share Price 1 Day Prior To Announcement',
+    'Target Share Price 1 Week Prior to Announcement ($)': 'Target Share Price 1 Week Prior To Announcement',
+    'Target Share Price 4 Weeks Prior to Announcement ($)': 'Target Share Price 4 Weeks Prior To Announcement',
+    ' Premium   1 day prior to announce-   ment   date': 'Premium 1 Day Prior To Announcement',
+    ' Premium  1 week prior to announce-   ment   date': 'Premium 1 Week Prior To Announcement',
+    ' Premium  4 weeks prior to ann. date': 'Premium 4 Weeks Prior To Announcement'
+},  inplace=True)
+
 print("---- Column Names and types ----")
+colnames = US_MERGER_DATA.columns
 for col in colnames:
     print("'" + col+"'", US_MERGER_DATA[col].dtypes)
-#
-#
+
+
 print("---- Shape ----")
 print(US_MERGER_DATA.shape)
 print("---- Null Count ----")
